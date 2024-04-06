db01d2c5c8d40cedfb23d26c39ff86575f99d827
Thone Waddy Khin <139044080+thawzinhtut1901@users.noreply.github.com>
2024-04-05 22:43:09 -0700

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,33 +2,33 @@ import { useState } from "react";
 import Form from "./Form";
 import Todo from "./Todo";
 import Edit from "./Edit";
-import {v4 as uuidv4}  from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 uuidv4()
 
 const TodoList = () => {
   const [todoValue , setTodo] = useState<any>([]);
 
-  const createToDo = todo => {
+  const createToDo = (todo : any) => {
     setTodo([...todoValue, {id: uuidv4(), task: todo, isEditing: false}])
   };
   
-  const deleteTodo = id => {
-    setTodo(todoValue.filter(todo => todo.id!== id))
+  const deleteTodo = (id : any) => {
+    setTodo(todoValue.filter((todo: any) => todo.id!== id))
   };
 
-  const editTodo = id => {
-    setTodo(todoValue.map(todo => todo.id === id ? {...todo, isEditing:!todo.isEditing}: todo))
+  const editTodo = (id : any) => {
+    setTodo(todoValue.map((todo : any) => todo.id === id ? {...todo, isEditing:!todo.isEditing}: todo))
   };
 
-  const editTask = (task, id) => (
-    setTodo(todoValue.map(todo => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo)  )
+  const editTask = (task : any, id : any) => (
+    setTodo(todoValue.map((todo : any) => todo.id === id ? {...todo, task, isEditing: !todo.isEditing} : todo)  )
   )
     return (
     <div className="bg-zinc-800 mx-[200px] p-8 rounded-md text-center">
       <Form createToDo = {createToDo}/>
       {
-        todoValue.map((todo, index) => (
+        todoValue.map((todo : any, index : any) => (
           todo.isEditing ? (
             <Edit key={index} editTodo={editTask} task={todo}/>
           ) : (<Todo task={todo} key={index} deleteTodo={deleteTodo} editTodo={editTodo}/>)
